a7e524f5b7774fc428678b02fda2b5883ed16d76
Virgile Vercellis <vmv92i@hotmail.fr>
2019-07-15 11:16:00 +0200

    Route

---
+++ src/Profil.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class Exemple extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
+
+    render() {
+        return (
+            <div>
+                <h1>Mon profil</h1>
+            </div>
+        );
+    }
+}
+
+export default Exemple;
